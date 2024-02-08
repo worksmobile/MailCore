e9a249acfd1b0b6556a6823e30f98705edb21158
intaek.cho <intaek.cho@navercorp.com>
2024-02-08 16:15:00 +0900

    SPM 지원

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MailCore",
+    products: [
+        .library(name: "MailCore", targets: ["MailCore"]),
+    ],
+    targets: [
+        .binaryTarget(name: "MailCore", path: "MailCore.xcframework")
+    ]
+)
